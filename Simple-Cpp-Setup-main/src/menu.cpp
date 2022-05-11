4c3a445a9515a74739d0bc5d5c14bae0f011b4f5
Sujet Test <atiusem@gmail.com>
2022-05-11 15:44:48 +0200

    [menu] Start change with Game struct

--- Simple-Cpp-Setup-main/src/menu.cpp
+++ Simple-Cpp-Setup-main/src/menu.cpp
@@ -2,17 +2,26 @@
 
 void command_list()
 {
+    std::cout << "* You wake up next to a campfire. A strange man is sitting in front of you. *" << std::endl;
     std::cout << "(old man voice) Welcome Traveler !" << std::endl;
     std::cout << "                   Would you like to play a game ?" << std::endl;
     std::cout << " 1. Play Guess a number" << std::endl;
     std::cout << " 2. Play Hangman" << std::endl;
-    std::cout << " 3. Rob the old man with a gun and steal his clothes" << std::endl;
+    std::cout << " 3. Play Nought and Crosses" << std::endl;
+    std::cout << " 4. Rob the old man with a gun and steal his clothes" << std::endl;
     std::cout << std::endl;
-    std::cout << " 4. Politely refuse and leave  (Quit)" << std::endl;
+    std::cout << " 5. Politely refuse and leave  (Quit)" << std::endl;
 }
 
 void menu()
 {
+    std::map<char, Game> game_map{
+        {'1', {"guess_a_number", guess_a_number()}},
+        {'2', {"hangman", hangman()}},
+        {'3', {"menudventure", menudventure()}},
+        {'4', {"nought_and_crosses", nought_and_crosses()}},
+    };
+
     bool quit = false;
     while (!quit) {
         command_list();
@@ -29,19 +38,20 @@ void menu()
             std::cout << std::endl;
             break;
 
-            // case '3':
-            //     menudventure();
-            //     break;
+        case '3':
+            nought_and_crosses();
+            break;
 
         case '4':
-            std::cout << "*The old man looks at you with a sad graze. He looks like a wet labrador." << std::endl;
-            std::cout << " You walk away, ignoring the pitying looks he gives you*" << std::endl
+            Menudventure::start();
+            break;
+
+        case '5':
+            std::cout << "*The old man looks at you extremely sad as you close your eyes*" << std::endl;
+            std::cout << std::endl
+                      << "Good night, traveler" << std::endl
                       << std::endl;
             quit = true;
         }
     }
-}
-
-void menudventure()
-{
 }
--- Simple-Cpp-Setup-main/src/menu.h
+++ Simple-Cpp-Setup-main/src/menu.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <iostream>
+#include <map>
 #include "general_store.h"
 #include "guess_a_number.h"
 #include "hangman.h"
-#include "menu.h"
+#include "menudventure.h"
+#include "nought_and_crosses.h"
+
+struct Game {
+    std::string           name;
+    std::function<void()> game;
+}
 
 void menu();
 void command_list();

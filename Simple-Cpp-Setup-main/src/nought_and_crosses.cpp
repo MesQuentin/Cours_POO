58a24d2ebe05bdc89b25b4dcb145ce7dfa1d9736
Sujet Test <atiusem@gmail.com>
2022-06-06 17:49:38 +0200

    [nought and crosses] Upgrade code + add lore

--- Simple-Cpp-Setup-main/src/nought_and_crosses.cpp
+++ Simple-Cpp-Setup-main/src/nought_and_crosses.cpp
@@ -1,5 +1,6 @@
 #include "nought_and_crosses.h"
 
+namespace NoughtAndCrosses {
 glm::vec2 get_cell_center(Cell cell, int board_size)
 {
     return p6::map(glm::vec2{static_cast<float>(cell.x),
@@ -47,20 +48,30 @@ void draw_cross(Cell cell, int board_size, p6::Context& ctx)
     ctx.rectangle(center, radii, -rotation);
 }
 
+void nought_or_crosses(Player player, Cell cell, int board_size, p6::Context& ctx)
+{
+    if (player == Player::Noughts) {
+        draw_nought(cell, board_size, ctx);
+    }
+    else {
+        draw_cross(cell, board_size, ctx);
+    }
+}
+
 template<int size>
-void draw_noughts_and_crosses(const Board<size>& board, p6::Context& ctx)
+void draw_symbols(const Board<size>& board, p6::Context& ctx)
 {
     for (int x = 0; x < size; ++x) {
         for (int y = 0; y < size; ++y) {
             const auto cell = board[{x, y}];
             if (cell.has_value()) {
-                draw_player(*cell, {x, y}, size, ctx);
+                nought_or_crosses(*cell, {x, y}, size, ctx);
             }
         }
     }
 }
 
-void change_player(Player& player)
+void next_player(Player& player)
 {
     if (player == Player::Noughts) {
         player = Player::Crosses;
@@ -89,16 +100,6 @@ std::optional<Cell> cell_hovered(glm::vec2 position, int board_size)
     }
 }
 
-void draw_player(Player player, Cell cell, int board_size, p6::Context& ctx)
-{
-    if (player == Player::Noughts) {
-        draw_nought(cell, board_size, ctx);
-    }
-    else {
-        draw_cross(cell, board_size, ctx);
-    }
-}
-
 template<int board_size>
 void try_to_play(std::optional<Cell> cell_index, Board<board_size>& board, Player& current_player)
 {
@@ -106,7 +107,7 @@ void try_to_play(std::optional<Cell> cell_index, Board<board_size>& board, Playe
         const auto cell_is_empty = !board[*cell_index].has_value();
         if (cell_is_empty) {
             board[*cell_index] = current_player;
-            change_player(current_player);
+            next_player(current_player);
         }
     }
 }
@@ -116,12 +117,12 @@ void try_draw_player_on_hovered_cell(Player player, Board<board_size> board, p6:
 {
     const auto hovered_cell = cell_hovered(ctx.mouse(), board_size);
     if (hovered_cell.has_value() && !board[*hovered_cell].has_value()) {
-        draw_player(player, *hovered_cell, board_size, ctx);
+        nought_or_crosses(player, *hovered_cell, board_size, ctx);
     }
 }
 
 template<int board_size>
-bool board_is_full(const Board<board_size>& board)
+bool full_board(const Board<board_size>& board)
 {
     return std::all_of(board.begin(), board.end(), [](const auto& cell) {
         return cell.has_value();
@@ -129,7 +130,7 @@ bool board_is_full(const Board<board_size>& board)
 }
 
 template<int board_size>
-std::optional<Player> check_for_winner_on_line(const Board<board_size>& board, std::function<Cell(int)> index_generator)
+std::optional<Player> line_winner(const Board<board_size>& board, std::function<Cell(int)> index_generator)
 {
     const bool are_all_equal = [&]() {
         for (int position = 0; position < board_size - 1; ++position) {
@@ -148,49 +149,50 @@ std::optional<Player> check_for_winner_on_line(const Board<board_size>& board, s
 }
 
 template<int board_size>
-std::optional<Player> check_for_winner(const Board<board_size>& board)
+std::optional<Player> is_there_winner(const Board<board_size>& board)
 {
     std::optional<Player> winner = std::nullopt;
-    // Columns
+
     for (int x = 0; x < board_size && !winner.has_value(); ++x) {
-        winner = check_for_winner_on_line(board, [x](int position) {
+        winner = line_winner(board, [x](int position) {
             return Cell{x, position};
         });
     }
-    // Rows
+
     for (int y = 0; y < board_size && !winner.has_value(); ++y) {
-        winner = check_for_winner_on_line(board, [y](int position) {
+        winner = line_winner(board, [y](int position) {
             return Cell{position, y};
         });
     }
-    // Diagonal
+
     if (!winner.has_value()) {
-        winner = check_for_winner_on_line(board, [](int position) {
+        winner = line_winner(board, [](int position) {
             return Cell{position, position};
         });
     }
-    // Anti-diagonal
+
     if (!winner.has_value()) {
-        winner = check_for_winner_on_line(board, [](int position) {
+        winner = line_winner(board, [](int position) {
             return Cell{position, board_size - position - 1};
         });
     }
+
     return winner;
 }
 
 template<int board_size>
-bool game_is_finished(const Board<board_size>& board)
+bool is_game_finished(const Board<board_size>& board)
 {
-    if (const auto winner = check_for_winner(board); winner.has_value()) {
+    if (const auto winner = is_there_winner(board); winner.has_value()) { // Dunno how it works, but it does
         if (*winner == Player::Noughts) {
-            std::cout << "Noughts have won!\n";
+            std::cout << " (old man voice) Well done, servant of the round table!\nThe order of crossed fates is in disarray and is not about to return any time soon! \n\n*The old man seems very happy with this result*";
         }
         else {
-            std::cout << "Crosses have won!\n";
+            std::cout << " (old man voice) And the order of crossed destinies wins this fight ... We can't always be on good days apparently. \nBut don't lose hope, knights, good will eventually triumph!\n*you have no idea what this old man is talking about* have won!\n";
         }
         return true;
     }
-    else if (board_is_full(board)) {
+    else if (full_board(board)) {
         std::cout << "This is a draw!\n";
         return true;
     }
@@ -198,6 +200,8 @@ bool game_is_finished(const Board<board_size>& board)
         return false;
     }
 }
+
+} // namespace NoughtAndCrosses
 
 void nought_and_crosses()
 {
@@ -207,18 +211,19 @@ void nought_and_crosses()
 
     auto ctx          = p6::Context{{1000, 1000, "Noughts and Crosses"}};
     ctx.mouse_pressed = [&](p6::MouseButton event) {
-        try_to_play(cell_hovered(event.position, board_size), board, current_player);
+        NoughtAndCrosses::try_to_play(NoughtAndCrosses::cell_hovered(event.position, board_size), board, current_player);
     };
     ctx.update = [&]() {
         ctx.background({0.85f, 0.55f, 0.19f, 1.f});
         ctx.stroke_weight = 0.01f;
         ctx.stroke        = {1.f, 1.f, 1.f, 1.f};
         ctx.fill          = {0.f, 0.f, 0.f, 0.f};
-        draw_board(board_size, ctx);
-        draw_noughts_and_crosses(board, ctx);
-        try_draw_player_on_hovered_cell(current_player, board, ctx);
 
-        if (game_is_finished(board)) {
+        NoughtAndCrosses::draw_board(board_size, ctx);
+        NoughtAndCrosses::draw_symbols(board, ctx);
+        NoughtAndCrosses::try_draw_player_on_hovered_cell(current_player, board, ctx);
+
+        if (NoughtAndCrosses::is_game_finished(board)) {
             ctx.stop();
         }
     };
